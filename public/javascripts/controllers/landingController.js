bc9a2fd49d9702d338bf9676a2bfb74599bedf79
sindhu hari <sindhuhari@sindhus-MacBook-Pro.local>
2016-06-10 12:35:22 -0700

    dynamic colors in landing-controller

--- public/javascripts/controllers/landingController.js
+++ public/javascripts/controllers/landingController.js
@@ -13,7 +13,6 @@ app.controller('LandingController', [
 	 * Init
 	 */
 	 $scope.tiles = [];
-
 	 // Generate 120 dummy tiles and asssign them to $scope.tiles
 	 for(var i = 0; i < 120; i++) {
 	 	var tile = {
@@ -28,8 +27,64 @@ app.controller('LandingController', [
 	  */
 
 	  $scope.setTheme = function() {
-	  	console.log("#### PRIMARY COLOR - " + $scope.primaryColor);
-	  	console.log("#### SECONDARY COLOR - " + $scope.secondaryColor);
+		  $scope.primaryColor = $scope.parseColor2($scope.primaryColor);
+		  $scope.secondaryColor = $scope.parseColor2($scope.secondaryColor);
+		  $scope.primaryBGStyle = {'background-color': $scope.primaryColor};
+		  $scope.secondaryBGStyle = {'background-color': $scope.secondaryColor};
 	  };
+		$scope.convertToRGB = function(c) {
+			return 'rgb(' + c[0] + ', ' + c[1] + ', ' + c[2] + ')';
+		};
 
+		// This doesnt work because we have different formats to support.
+		$scope.parseColor = function(input) {
+			var div = document.createElement('div'), m;
+			div.style.color = input;
+			m = getComputedStyle(div).color.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+			if( m) return [m[1],m[2],m[3]];
+			else throw new Error("Colour "+input+" could not be parsed.");
+		};
+		$scope.parseColor2 = function(c) {
+			    if (!c) {
+					return null;
+				}
+				c= c.toLowerCase().trim();
+				if (/^[a-z]+$/.test(c)){
+					var colornames={
+						aqua:'#00ffff', black:'#000000', blue:'#0000ff', fuchsia:'#ff00ff',
+						gray:'#808080', green:'#008000', lime:'#00ff00', maroon:'#800000',
+						navy:'#000080', olive:'#808000', orange:'#ffa500', purple:'#800080',
+						red:'#ff0000', silver:'#c0c0c0', teal:'#008080', white:'#ffffff',
+						yellow:'#ffff00'
+					};
+					c= colornames[c];
+				}
+			    // If # is missing, append it.
+				if(/^([a-f0-9]{3}){1,2}$/.test(c)) {
+					c = "#" + c;
+				} else if (/\d+.,.\d+.,.\d+/.test(c)) {
+					c = 'rgb' + c;
+				}
+			    console.log(">>> c is ", c);
+				if(/^#([a-f0-9]{3}){1,2}$/.test(c)){
+					if(c.length== 4){
+						c= '#'+[c[1], c[1], c[2], c[2], c[3], c[3]].join('');
+					}
+					c= '0x'+c.substring(1);
+					return $scope.convertToRGB([(c>>16)&255, (c>>8)&255, c&255 ]);
+				} else if(c.indexOf('rgb')== 0){
+					c= c.match(/\d+(\.\d+)?%?/g);
+					if(c){
+						for(var i= 0;i<3;i++){
+							if(c[i].indexOf('%')!= -1){
+								c[i]= Math.round(parseFloat(c[i])*2.55);
+							}
+							if(c[i]<0) c[i]= 0;
+							if(c[i]>255) c[i]= 255;
+						}
+						return $scope.convertToRGB(c);
+					}
+				}
+			return c;
+		}
 }]);
